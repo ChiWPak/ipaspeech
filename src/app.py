5dc7f85c3f6244975fa66ab173b33da6a18cf318
Chi Pak <cp@chis-mbp.lan>
2024-09-16 23:06:44 -0500

    amazon polly ipa pronunciation

---
+++ src/app.py
@@ -0,0 +1,68 @@
+"""API entry point
+"""
+
+from fastapi import (
+    Depends,
+    FastAPI,
+    Form,
+    HTTPException,
+    Path,
+    Request
+)
+from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+
+from tts.polly import Polly
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI(
+    title="IPA",
+    summary="IPA speech",
+    docs_url="/swagger",
+    redoc_url="/docs"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", response_class=RedirectResponse, include_in_schema=False)
+def root():
+    """Docs"""
+    return "/docs"
+
+
+@app.get("/ping", status_code=200, include_in_schema=False)
+def ping():
+    """Health check"""
+    return {"response": "pong"}
+
+
+@app.get("/home", status_code=200, include_in_schema=False)
+async def homepage(request: Request):
+    """Home page"""
+    return templates.TemplateResponse("index.html", context={"request": request})
+
+
+def get_tts_client(provider: str = Path(...)):
+    """Get TTS client based on provider"""
+    if provider == "polly":
+        tts_client = Polly()
+    else:
+        raise HTTPException(f"Provider {provider} is not supported.")
+    return tts_client
+
+
+@app.post("/tts/{provider}", status_code=200, response_class=FileResponse, include_in_schema=False)
+async def polly(text: str = Form(...), tts_client = Depends(get_tts_client)):
+    """Polly text to speech"""
+    tts_client.tts(text=text)
+    return FileResponse(path=tts_client.audio_path, media_type="mp3")
---
+++ src/tts/polly.py
@@ -0,0 +1,60 @@
+"""Wrapper around Polly client
+"""
+
+from __future__ import annotations
+
+import os
+from functools import cached_property
+
+import boto3
+
+
+POLLY_HOME_PATH = os.getenv("POLLY_HOME_PATH")
+
+
+polly_client = boto3.client("polly", region_name="us-east-1")
+
+
+class Polly:
+    """Wrapper around Polly boto3 client
+    """
+
+    def __init__(self):
+        pass
+
+    @cached_property
+    def polly(self):
+        """boto3 Polly client
+        """
+        return polly_client
+
+    def tts(
+            self,
+            text: str,
+            engine: str = "generative",
+            output_format: str = "mp3",
+            text_type: str = "ssml",
+            voice_id: str = "Ruth",
+        ) -> dict:
+        """Make Polly synthesize speech call
+        """
+        response = self.polly.synthesize_speech(
+            Engine=engine,
+            OutputFormat=output_format,
+            Text=text,
+            TextType=text_type,
+            VoiceId=voice_id,
+        )
+        self._write(response)
+        return response
+
+    def _write(self, response: dict) -> None:
+        """Save audio response to file
+        """
+        with open(self.audio_path, "wb") as f:
+            f.write(response["AudioStream"].read())
+
+    @cached_property
+    def audio_path(self) -> str:
+        """Audio path"""
+        return f"{POLLY_HOME_PATH}/speech.mp3"
